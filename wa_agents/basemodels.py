7bd26579dcd6a473570a65090da4a461078f5aeb
luis-i-reyes-castro <luis.i.reyes.castro@gmail.com>
2026-05-13 16:39:19 -0500

    In `ServerInteractiveOptsMsg` add warning for long button text

--- wa_agents/basemodels.py
+++ wa_agents/basemodels.py
@@ -787,6 +787,10 @@ class ServerInteractiveOptsMsg( ServerMsg, StructuredDataMsg) :
             e_msg += "Type 'list' only supports up to 10 options"
             raise ValueError(e_msg)
         
+        if self.button and len(self.button > 20) :
+            e_msg += "Field 'button' supports a max length of 20 chars"
+            raise ValueError(e_msg)
+        
         return self
     
     def as_text(self) -> str :
