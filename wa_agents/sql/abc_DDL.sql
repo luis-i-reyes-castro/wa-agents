d3752865188166e252afc596b63275c621f0567a
luis-i-reyes-castro <luis.i.reyes.castro@gmail.com>
2026-09-09 11:13:31 -0500

    Add table to relate messages between API and Case Handler

--- wa_agents/sql/abc_DDL.sql
+++ wa_agents/sql/abc_DDL.sql
@@ -54,7 +54,7 @@ CREATE TYPE T_WHATSAPP_STATUS AS ENUM (
 -- ========================================================================================
 -- BUSINESSES & USERS
 
-CREATE TABLE IF NOT EXISTS public.wa_businesses (
+CREATE TABLE IF NOT EXISTS public.wa_api_businesses (
   
   id            BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   created_at    TIMESTAMPTZ NOT NULL DEFAULT now(),
@@ -64,18 +64,18 @@ CREATE TABLE IF NOT EXISTS public.wa_businesses (
   phone_number_id       T_NUMERIC_ID NOT NULL,
   display_phone_number  T_NUMERIC_ID NOT NULL,
   
-  CONSTRAINT wa_businesses_phone_number_id_unique
+  CONSTRAINT wa_api_businesses_phone_number_id_unique
     UNIQUE (phone_number_id),
   
-  CONSTRAINT wa_businesses_display_phone_number_unique
+  CONSTRAINT wa_api_businesses_display_phone_number_unique
     UNIQUE (display_phone_number)
 
 );
 
-ALTER TABLE public.wa_businesses
+ALTER TABLE public.wa_api_businesses
   ENABLE ROW LEVEL SECURITY;
 
-CREATE TABLE IF NOT EXISTS public.wa_contacts (
+CREATE TABLE IF NOT EXISTS public.wa_api_contacts (
   
   id            BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   created_at    TIMESTAMPTZ NOT NULL DEFAULT now(),
@@ -88,33 +88,33 @@ CREATE TABLE IF NOT EXISTS public.wa_contacts (
   profile_name      TEXT                DEFAULT NULL,
   profile_username  T_WHATSAPP_USERNAME DEFAULT NULL,
   
-  CONSTRAINT wa_contacts_business_fkey
+  CONSTRAINT wa_api_contacts_business_fkey
     FOREIGN KEY (business)
-    REFERENCES public.wa_businesses(id)
+    REFERENCES public.wa_api_businesses(id)
     ON UPDATE CASCADE
     ON DELETE CASCADE,
   
-  CONSTRAINT wa_contacts_either_wa_id_or_user_id
+  CONSTRAINT wa_api_contacts_either_wa_id_or_user_id
     CHECK(
       ( wa_id IS NOT NULL ) OR ( user_id IS NOT NULL )
     ),
   
-  CONSTRAINT wa_contacts_business_wa_id_unique
+  CONSTRAINT wa_api_contacts_business_wa_id_unique
     UNIQUE ( business, wa_id),
   
-  CONSTRAINT wa_contacts_business_user_id_unique
+  CONSTRAINT wa_api_contacts_business_user_id_unique
     UNIQUE ( business, user_id)
 
 );
 
-ALTER TABLE public.wa_contacts
+ALTER TABLE public.wa_api_contacts
   ENABLE ROW LEVEL SECURITY;
 
 
 -- ========================================================================================
 -- PAYLOADS
 
-CREATE TABLE IF NOT EXISTS public.wa_inbound_payload_metadata (
+CREATE TABLE IF NOT EXISTS public.wa_api_inbound_payload_metadata (
   
   id            BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   received_at   TIMESTAMPTZ       NOT NULL DEFAULT now(),
@@ -126,31 +126,31 @@ CREATE TABLE IF NOT EXISTS public.wa_inbound_payload_metadata (
   contact       BIGINT            NOT NULL,
   payload_hash  T_SHA256_HEX_HASH NOT NULL,
   
-  CONSTRAINT wa_inbound_payload_metadata_contact_fkey
+  CONSTRAINT wa_api_inbound_payload_metadata_contact_fkey
     FOREIGN KEY (contact)
-    REFERENCES public.wa_contacts(id)
+    REFERENCES public.wa_api_contacts(id)
     ON UPDATE CASCADE
     ON DELETE CASCADE,
   
-  CONSTRAINT wa_inbound_payload_metadata_hash_unique
+  CONSTRAINT wa_api_inbound_payload_metadata_hash_unique
     UNIQUE (payload_hash)
 
 );
 
-CREATE INDEX IF NOT EXISTS wa_inbound_payload_metadata_received_at_idx
-  ON public.wa_inbound_payload_metadata (received_at);
+CREATE INDEX IF NOT EXISTS wa_api_inbound_payload_metadata_received_at_idx
+  ON public.wa_api_inbound_payload_metadata (received_at);
 
-CREATE INDEX IF NOT EXISTS wa_inbound_payload_metadata_contact_idx
-  ON public.wa_inbound_payload_metadata (contact);
+CREATE INDEX IF NOT EXISTS wa_api_inbound_payload_metadata_contact_idx
+  ON public.wa_api_inbound_payload_metadata (contact);
 
-ALTER TABLE public.wa_inbound_payload_metadata
+ALTER TABLE public.wa_api_inbound_payload_metadata
   ENABLE ROW LEVEL SECURITY;
 
 
 -- ========================================================================================
 -- MESSAGES: INBOUND
 
-CREATE TABLE IF NOT EXISTS public.wa_inbound_messages (
+CREATE TABLE IF NOT EXISTS public.wa_api_inbound_messages (
   
   id            BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   payload       BIGINT                NOT NULL,
@@ -161,26 +161,26 @@ CREATE TABLE IF NOT EXISTS public.wa_inbound_messages (
   msg_type      T_WHATSAPP_MESSAGE    NOT NULL,
   msg_data      JSONB                 NOT NULL,
   
-  CONSTRAINT wa_inbound_messages_payload_fkey
+  CONSTRAINT wa_api_inbound_messages_payload_fkey
     FOREIGN KEY (payload)
-    REFERENCES public.wa_inbound_payload_metadata(id)
+    REFERENCES public.wa_api_inbound_payload_metadata(id)
     ON UPDATE CASCADE
     ON DELETE CASCADE,
   
-  CONSTRAINT wa_inbound_messages_msg_id_unique
+  CONSTRAINT wa_api_inbound_messages_msg_id_unique
     UNIQUE (msg_id)
 
 );
 
-CREATE INDEX IF NOT EXISTS wa_inbound_messages_payload_idx
-  ON public.wa_inbound_messages (payload);
+CREATE INDEX IF NOT EXISTS wa_api_inbound_messages_payload_idx
+  ON public.wa_api_inbound_messages (payload);
 
-ALTER TABLE public.wa_inbound_messages
+ALTER TABLE public.wa_api_inbound_messages
   ENABLE ROW LEVEL SECURITY;
 
 -- MESSAGES: OUTBOUND
 
-CREATE TABLE IF NOT EXISTS public.wa_outbound_messages (
+CREATE TABLE IF NOT EXISTS public.wa_api_outbound_messages (
   
   id            BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   sent_at       TIMESTAMPTZ           NOT NULL DEFAULT now(),
@@ -190,73 +190,73 @@ CREATE TABLE IF NOT EXISTS public.wa_outbound_messages (
   msg_type      T_WHATSAPP_MESSAGE    NOT NULL,
   msg_data      JSONB                 NOT NULL,
   
-  CONSTRAINT wa_outbound_messages_contact_fkey
+  CONSTRAINT wa_api_outbound_messages_contact_fkey
     FOREIGN KEY (contact)
-    REFERENCES public.wa_contacts(id)
+    REFERENCES public.wa_api_contacts(id)
     ON UPDATE CASCADE
     ON DELETE CASCADE,
   
-  CONSTRAINT wa_outbound_messages_msg_id_unique
+  CONSTRAINT wa_api_outbound_messages_msg_id_unique
     UNIQUE (msg_id)
 
 );
 
-CREATE INDEX IF NOT EXISTS wa_outbound_messages_contact_idx
-  ON public.wa_outbound_messages (contact);
+CREATE INDEX IF NOT EXISTS wa_api_outbound_messages_contact_idx
+  ON public.wa_api_outbound_messages (contact);
 
-ALTER TABLE public.wa_outbound_messages
+ALTER TABLE public.wa_api_outbound_messages
   ENABLE ROW LEVEL SECURITY;
 
 -- MESSAGES: MEDIA
 
-CREATE TABLE IF NOT EXISTS public.wa_media (
+CREATE TABLE IF NOT EXISTS public.wa_api_media (
   
-  id            BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  inbound_id    BIGINT    DEFAULT NULL,
-  outbound_id   BIGINT    DEFAULT NULL,
+  id                BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  inbound_msg_id    BIGINT    DEFAULT NULL,
+  outbound_msg_id   BIGINT    DEFAULT NULL,
   
-  mime_type     TEXT      NOT NULL,
-  size          BIGINT    NOT NULL,
-  prefix        T_NE_STR  NOT NULL,
-  caption       TEXT      DEFAULT NULL,
-  filename      TEXT      DEFAULT NULL,
+  mime_type         TEXT      NOT NULL,
+  size              BIGINT    NOT NULL,
+  prefix            T_NE_STR  NOT NULL,
+  caption           TEXT      DEFAULT NULL,
+  filename          TEXT      DEFAULT NULL,
   
-  CONSTRAINT wa_media_inbound_id_fkey
-    FOREIGN KEY (inbound_id)
-    REFERENCES public.wa_inbound_messages(id)
+  CONSTRAINT wa_api_media_inbound_msg_id_fkey
+    FOREIGN KEY (inbound_msg_id)
+    REFERENCES public.wa_api_inbound_messages(id)
     ON UPDATE CASCADE
     ON DELETE CASCADE,
   
-  CONSTRAINT wa_media_outbound_id_fkey
-    FOREIGN KEY (outbound_id)
-    REFERENCES public.wa_outbound_messages(id)
+  CONSTRAINT wa_api_media_outbound_msg_id_fkey
+    FOREIGN KEY (outbound_msg_id)
+    REFERENCES public.wa_api_outbound_messages(id)
     ON UPDATE CASCADE
     ON DELETE CASCADE,
   
-  CONSTRAINT wa_media_inbound_id_unique
-    UNIQUE (inbound_id),
+  CONSTRAINT wa_api_media_inbound_msg_id_unique
+    UNIQUE (inbound_msg_id),
   
-  CONSTRAINT wa_media_outbound_id_unique
-    UNIQUE (outbound_id),
+  CONSTRAINT wa_api_media_outbound_msg_id_unique
+    UNIQUE (outbound_msg_id),
   
-  CONSTRAINT wa_media_either_inbound_or_outbound
+  CONSTRAINT wa_api_media_either_inbound_or_outbound
     CHECK (
-      ( inbound_id IS NOT NULL ) <> ( outbound_id IS NOT NULL )
+      ( inbound_msg_id IS NOT NULL ) <> ( outbound_msg_id IS NOT NULL )
     ),
   
-  CONSTRAINT wa_media_size_nonnegative
+  CONSTRAINT wa_api_media_size_nonnegative
     CHECK ( size >= 0 )
 
 );
 
-ALTER TABLE public.wa_media
+ALTER TABLE public.wa_api_media
   ENABLE ROW LEVEL SECURITY;
 
 
 -- ========================================================================================
 -- STATUSES OF SENT MESSAGES
 
-CREATE TABLE IF NOT EXISTS public.wa_statuses (
+CREATE TABLE IF NOT EXISTS public.wa_api_statuses (
   
   id            BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   payload       BIGINT                NOT NULL,
@@ -269,27 +269,27 @@ CREATE TABLE IF NOT EXISTS public.wa_statuses (
   pricing       JSONB DEFAULT NULL,
   errors        JSONB DEFAULT NULL,
   
-  CONSTRAINT wa_statuses_payload_fkey
+  CONSTRAINT wa_api_statuses_payload_fkey
     FOREIGN KEY (payload)
-    REFERENCES public.wa_inbound_payload_metadata(id)
+    REFERENCES public.wa_api_inbound_payload_metadata(id)
     ON UPDATE CASCADE
     ON DELETE CASCADE,
   
-  CONSTRAINT wa_statuses_msg_id_fkey
+  CONSTRAINT wa_api_statuses_msg_id_fkey
     FOREIGN KEY (msg_id)
-    REFERENCES public.wa_outbound_messages(msg_id)
+    REFERENCES public.wa_api_outbound_messages(msg_id)
     ON UPDATE CASCADE
     ON DELETE CASCADE
 
 );
 
-CREATE INDEX IF NOT EXISTS wa_statuses_payload_idx
-  ON public.wa_statuses (payload);
+CREATE INDEX IF NOT EXISTS wa_api_statuses_payload_idx
+  ON public.wa_api_statuses (payload);
 
-CREATE INDEX IF NOT EXISTS wa_statuses_msg_id_idx
-  ON public.wa_statuses (msg_id);
+CREATE INDEX IF NOT EXISTS wa_api_statuses_msg_id_idx
+  ON public.wa_api_statuses (msg_id);
 
-ALTER TABLE public.wa_statuses
+ALTER TABLE public.wa_api_statuses
   ENABLE ROW LEVEL SECURITY;
 
 
@@ -306,7 +306,7 @@ CREATE TYPE T_ENQUEUED_MESSAGE_STATUS AS ENUM (
   'processing'
 );
 
-CREATE TABLE IF NOT EXISTS public.wa_inbound_messages_queue (
+CREATE TABLE IF NOT EXISTS public.wa_api_to_case_handler_queue (
   
   id            BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   created_at    TIMESTAMPTZ               NOT NULL DEFAULT now(),
@@ -316,37 +316,37 @@ CREATE TABLE IF NOT EXISTS public.wa_inbound_messages_queue (
   msg_id        T_WHATSAPP_MESSAGE_ID     NOT NULL,
   msg_status    T_ENQUEUED_MESSAGE_STATUS NOT NULL DEFAULT 'pending',
   
-  CONSTRAINT wa_inbound_messages_queue_msg_id_fkey
+  CONSTRAINT wa_api_to_case_handler_queue_msg_id_fkey
     FOREIGN KEY (msg_id)
-    REFERENCES public.wa_inbound_messages(msg_id)
+    REFERENCES public.wa_api_inbound_messages(msg_id)
     ON UPDATE CASCADE
     ON DELETE CASCADE,
   
-  CONSTRAINT wa_inbound_messages_queue_msg_id_unique
+  CONSTRAINT wa_api_to_case_handler_queue_msg_id_unique
     UNIQUE (msg_id)
 
 );
 
-CREATE INDEX IF NOT EXISTS wa_inbound_messages_queue_status_idx
-  ON public.wa_inbound_messages_queue (
+CREATE INDEX IF NOT EXISTS wa_api_to_case_handler_queue_status_idx
+  ON public.wa_api_to_case_handler_queue (
     msg_status,
     created_at,
     id
   );
 
-ALTER TABLE public.wa_inbound_messages_queue
+ALTER TABLE public.wa_api_to_case_handler_queue
   ENABLE ROW LEVEL SECURITY;
 
 
 /*
   =========================================================================================
-  WA-AGENTS
+  CASE HANDLER
   =========================================================================================
 */
 
 -- CASE MANIFESTS
 
-CREATE TABLE IF NOT EXISTS public.wa_agents_cases (
+CREATE TABLE IF NOT EXISTS public.wa_case_handler_case_manifests (
   
   id              BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
   created_at      TIMESTAMPTZ   NOT NULL DEFAULT now(),
@@ -356,54 +356,102 @@ CREATE TABLE IF NOT EXISTS public.wa_agents_cases (
   is_open         BOOLEAN       NOT NULL DEFAULT TRUE,
   machine_state   T_NE_STR      DEFAULT NULL,
   
-  CONSTRAINT wa_agents_cases_contact_fkey
+  CONSTRAINT wa_case_handler_case_manifests_contact_fkey
     FOREIGN KEY (contact)
-    REFERENCES public.wa_contacts(id)
+    REFERENCES public.wa_api_contacts(id)
     ON UPDATE CASCADE
     ON DELETE CASCADE
 
 );
 
-CREATE INDEX IF NOT EXISTS wa_agents_cases_contact_idx
-  ON public.wa_agents_cases (contact);
+CREATE INDEX IF NOT EXISTS wa_case_handler_case_manifests_contact_idx
+  ON public.wa_case_handler_case_manifests (contact);
 
-CREATE UNIQUE INDEX IF NOT EXISTS wa_agents_cases_contact_open_idx
-  ON public.wa_agents_cases (contact)
+CREATE UNIQUE INDEX IF NOT EXISTS wa_case_handler_case_manifests_contact_open_idx
+  ON public.wa_case_handler_case_manifests (contact)
   WHERE is_open;
 
-ALTER TABLE public.wa_agents_cases
+ALTER TABLE public.wa_case_handler_case_manifests
   ENABLE ROW LEVEL SECURITY;
 
 -- MESSAGES
 
-CREATE TABLE IF NOT EXISTS public.wa_agents_messages (
+CREATE TABLE IF NOT EXISTS public.wa_case_handler_messages (
   
   id            BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  created_at    TIMESTAMPTZ NOT NULL DEFAULT now(),
-  received_at   TIMESTAMPTZ DEFAULT NULL,
+  created_at    TIMESTAMPTZ   NOT NULL DEFAULT now(),
+  received_at   TIMESTAMPTZ   DEFAULT NULL,
   
-  case_id       BIGINT      NOT NULL,
-  basemodel     T_NE_STR    NOT NULL,
-  origin        T_NE_STR    DEFAULT NULL,
-  data          JSONB       NOT NULL,
+  case_id       BIGINT        NOT NULL,
+  basemodel     T_NE_STR      NOT NULL,
+  origin        T_NE_STR      DEFAULT NULL,
+  data          JSONB         NOT NULL,
   
-  CONSTRAINT wa_agents_messages_case_id_fkey
+  CONSTRAINT wa_case_handler_messages_case_id_fkey
     FOREIGN KEY (case_id)
-    REFERENCES public.wa_agents_cases(id)
+    REFERENCES public.wa_case_handler_case_manifests(id)
     ON UPDATE CASCADE
     ON DELETE CASCADE,
   
-  CONSTRAINT wa_agents_messages_data_object
+  CONSTRAINT wa_case_handler_messages_data_object
     CHECK ( jsonb_typeof(data) = 'object' )
 
 );
 
-CREATE INDEX IF NOT EXISTS wa_agents_messages_case_order_idx
-  ON public.wa_agents_messages (
+CREATE INDEX IF NOT EXISTS wa_case_handler_messages_case_order_idx
+  ON public.wa_case_handler_messages (
     case_id,
     created_at,
     id
   );
 
-ALTER TABLE public.wa_agents_messages
+ALTER TABLE public.wa_case_handler_messages
+  ENABLE ROW LEVEL SECURITY;
+
+-- WHATSAPP MESSAGE IDS
+
+CREATE TABLE IF NOT EXISTS public.wa_case_handler_to_api (
+  
+  id          BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  created_at  TIMESTAMPTZ NOT NULL   DEFAULT now(),
+  
+  case_handler_msg_id   BIGINT NOT NULL,
+  api_inbound_msg_id    BIGINT DEFAULT NULL,
+  api_outbound_msg_id   BIGINT DEFAULT NULL,
+  
+  CONSTRAINT wa_case_handler_to_api_case_handler_msg_id_fkey
+    FOREIGN KEY (case_handler_msg_id)
+    REFERENCES public.wa_case_handler_messages(id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE,
+  
+  CONSTRAINT wa_case_handler_to_api_api_inbound_msg_id_fkey
+    FOREIGN KEY (api_inbound_msg_id)
+    REFERENCES public.wa_api_inbound_messages(id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE,
+  
+  CONSTRAINT wa_case_handler_to_api_api_outbound_msg_id_fkey
+    FOREIGN KEY (api_outbound_msg_id)
+    REFERENCES public.wa_api_outbound_messages(id)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE,
+  
+  CONSTRAINT wa_case_handler_to_api_case_handler_msg_id_unique
+    UNIQUE (case_handler_msg_id),
+  
+  CONSTRAINT wa_case_handler_to_api_api_inbound_msg_id_unique
+    UNIQUE (api_inbound_msg_id),
+  
+  CONSTRAINT wa_case_handler_to_api_api_outbound_msg_id_unique
+    UNIQUE (api_outbound_msg_id),
+  
+  CONSTRAINT wa_case_handler_to_api_either_inbound_or_outbound
+    CHECK (
+      ( api_inbound_msg_id IS NOT NULL ) <> ( api_outbound_msg_id IS NOT NULL )
+    )
+
+);
+
+ALTER TABLE public.wa_case_handler_to_api
   ENABLE ROW LEVEL SECURITY;
